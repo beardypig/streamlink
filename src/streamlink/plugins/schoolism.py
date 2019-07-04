1bb427d245b5bf490acd85de8fc3e232b2356185
beardypig <beardypig@protonmail.com>
2019-07-04 15:14:15 +0200

    wip

--- src/streamlink/plugins/schoolism.py
+++ src/streamlink/plugins/schoolism.py
@@ -8,6 +8,7 @@ from streamlink.plugin import Plugin, PluginArguments, PluginArgument
 from streamlink.plugin.api import useragents
 from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream, HTTPStream
+from streamlink.stream.ffmpegmux import MuxedStream
 from streamlink.utils import parse_json
 
 log = logging.getLogger(__name__)
@@ -35,10 +36,13 @@ class Schoolism(Plugin):
                         "title": validate.text,
                         "src": validate.text,
                         "type": validate.text,
-                    }],
-                        # only include HLS streams
-                        # validate.filter(lambda s: s["type"] == "application/x-mpegurl")
-                    )
+                    }]),
+                    validate.optional("subtitles"): validate.all([{
+                        "default": bool,
+                        "kind": validate.text,
+                        "srclang": validate.text,
+                        "src": validate.url()
+                    }])
                 }]
             )
         )
@@ -70,6 +74,15 @@ class Schoolism(Plugin):
         Play part number PART of the lesson, or assignment feedback video.
 
         Defaults is 1.
+        """),
+        PluginArgument(
+            "mux-subtitles",
+            type="store_true",
+            default=False,
+            help="""
+        If there are subtitles available, then mux them in to output stream.
+
+        Defaults is disabled.
         """
         )
     )
@@ -98,6 +111,21 @@ class Schoolism(Plugin):
         else:
             log.error("An email and password are required to access Schoolism streams")
 
+    def _extract_streams(self, sources):
+        for source in sources:
+            if source['type'] == "video/mp4":
+                yield "live", HTTPStream(self.session, source["src"],
+                                    headers={"User-Agent": useragents.SAFARI_8,
+                                             "Referer": self.url})
+
+
+            elif source['type'] == "application/x-mpegurl":
+                for q, s in HLSStream.parse_variant_playlist(self.session,
+                                                             source["src"],
+                                                             headers={"User-Agent": useragents.SAFARI_8,
+                                                                      "Referer": self.url}).items():
+                    yield q, s
+
     def _get_streams(self):
         user = self.login(self.options.get("email"), self.options.get("password"))
         if user:
@@ -109,28 +137,34 @@ class Schoolism(Plugin):
             video_type = "Lesson" if "lesson" in self.url_re.match(self.url).group(1).lower() else "Assignment Feedback"
 
             log.info("Attempting to play {0} Part {1}", video_type, part)
-            found = False
 
             # make request to key-time api, to get key specific headers
             _ = self.session.http.get(self.key_time_url, headers={"User-Agent": useragents.SAFARI_8})
 
-            for i, video in enumerate(lesson_playlist, 1):
-                if video["sources"] and i == part:
-                    found = True
-                    for source in video["sources"]:
-                        if source['type'] == "video/mp4":
-                            yield "live", HTTPStream(self.session, source["src"],
-                                                     headers={"User-Agent": useragents.SAFARI_8,
-                                                              "Referer": self.url})
-                        elif source['type'] == "application/x-mpegurl":
-                            for s in HLSStream.parse_variant_playlist(self.session,
-                                                                      source["src"],
-                                                                      headers={"User-Agent": useragents.SAFARI_8,
-                                                                               "Referer": self.url}).items():
-                                yield s
+            try:
+                video = lesson_playlist[part+1]
+            except IndexError:
+                log.error("Could not find {0} Part {1} (there are only {2} parts)", video_type, part, len(lesson_playlist))
+            else:
+                if "sources" not in video:
+                    log.error("Could not find {0} Part {1}", video_type, part)
+                else:
+                    subtitles = None
+                    stream_metadata = None
+                    disposition = None
+                    if self.options.get("mux_subtitles"):
+                        if "subtitles" in video:
+                            # extract subtitles
+                            pass
+                        else:
+                            log.warning("Subtitles requested, but none are available")
 
-            if not found:
-                log.error("Could not find {0} Part {1}", video_type, part)
+                    for quality, stream in self._extract_streams(video["sources"]):
+                        if subtitles:
+                            yield quality, MuxedStream(self.session, stream, subtitles, metadata=stream_metadata, disposition=disposition)
+                        else:
+                            yield quality, stream
+
 
 
 __plugin__ = Schoolism
