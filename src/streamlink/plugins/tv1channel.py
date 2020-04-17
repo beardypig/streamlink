3026b15e67235883071b74d71e8b4612d6ba27d2
beardypig <beardypig@protonmail.com>
2020-04-17 11:33:28 +0200

    plugins.tv1channel: update for page layout change

--- src/streamlink/plugins/tv1channel.py
+++ src/streamlink/plugins/tv1channel.py
@@ -1,3 +1,4 @@
+import logging
 import re
 
 from streamlink.plugin import Plugin
@@ -5,9 +6,12 @@ from streamlink.plugin.api import useragents
 from streamlink.plugin.api.utils import itertags
 from streamlink.stream import HLSStream
 
+log = logging.getLogger(__name__)
+
 
 class TV1Channel(Plugin):
     _url_re = re.compile(r'https?://(?:www\.)?tv1channel\.org/(?!play/)(?:index\.php/livetv)?')
+    _hls_load = re.compile(r'''hls.loadSource\((?P<quote>["']?)(?P<url>http.*?)(?P=quote)\)''')
 
     @classmethod
     def can_handle_url(cls, url):
@@ -20,13 +24,17 @@ class TV1Channel(Plugin):
             if 'cdn.netbadgers.com' not in iframe.attributes.get('src'):
                 continue
 
-            res = self.session.http.get(iframe.attributes.get('src'))
-            for source in itertags(res.text, 'source'):
-                if source.attributes.get('src') and source.attributes.get('src').endswith('.m3u8'):
-                    return HLSStream.parse_variant_playlist(self.session,
-                                                            source.attributes.get('src'))
+            iframe_url = iframe.attributes.get('src')
+            log.debug("Found iframe: {0}".format(iframe_url))
+            res = self.session.http.get(iframe_url)
 
-            break
+            m = self._hls_load.search(res.text)
+            if m:
+                return HLSStream.parse_variant_playlist(self.session, m.group("url"))
+            else:
+                log.debug("Could not find hls.loadSource call")
+
+        log.debug("Could not find expected iframe src")
 
 
 __plugin__ = TV1Channel
